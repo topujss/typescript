81f8546c36daae4da1818af42e4b2245791f75cf
topujss <adahmedt@gmail.com>
2023-05-05 23:56:29 -0400

    typescript learn

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict";
+/**
+ * @description TypeScript type learn
+ */
+// let fullName: string = 'John Doe';
+// let age: number = 20;
+// let isAdmin: boolean = false;
+// let sentence = `Hello, my name is ${fullName}. I'll be ${age + 1} years old next year. I'm ${
+//   isAdmin ? 'an' : 'not an'
+// } admin.`;
+// age = 21;
+// console.log(sentence);
+Object.defineProperty(exports, "__esModule", { value: true });
+/**
+ * @description TypeScript union type learn
+ */
+var devs;
+devs = 'true';
+console.log(devs);
---
+++ main.ts
@@ -0,0 +1,23 @@
+/**
+ * @description TypeScript type learn
+ */
+// let fullName: string = 'John Doe';
+// let age: number = 20;
+// let isAdmin: boolean = false;
+// let sentence = `Hello, my name is ${fullName}. I'll be ${age + 1} years old next year. I'm ${
+//   isAdmin ? 'an' : 'not an'
+// } admin.`;
+// age = 21;
+// console.log(sentence);
+
+/**
+ * @description TypeScript union type learn
+ */
+let devs: boolean | number | string;
+
+devs = 'true';
+
+console.log(devs);
+
+// export
+export {};
