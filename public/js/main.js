b172ceedd6e66022afbd596c64643cccef1f36d1
topujss <adahmedt@gmail.com>
2023-06-21 21:32:32 -0400

    ts revise again

--- public/js/main.js
+++ public/js/main.js
@@ -1,83 +1,111 @@
 "use strict";
-/**
- * @description TypeScript type learn
+/** Basic to know before
+ * => when there is config file just command in terminal: tsc -w
+ * => never use 'any' type unless needed cuz it's like javascript type.
+ * => when using typeScript always use explicit type not implicit.
  */
-// let fullName: string = 'John Doe';
-// let age: number = 20;
-// let isAdmin: boolean = false;
-// let sentence = `Hello, my name is ${fullName}. I'll be ${age + 1} years old next year. I'm ${
-//   isAdmin ? 'an' : 'not an'
-// } admin.`;
-// age = 21;
+/**
+ * @desc TypeScript type learn
+ */
+// let fullname: string = `toquir ahmed topu`;
+// let age: number = 30;
+// let skill: string = 'software developer';
+// let isSkilled: boolean = false;
+// const sentence = `My name is ${fullname} and i am ${age} years old. I am a ${skill}. I am a ${
+//   isSkilled ? 'skill' : 'exploration'
+// } person.`;
 // console.log(sentence);
 /**
- * @Union in TypeScript learn
+ * @Union dataType learn in TypeScript learn
  */
-// let devs: boolean | number | string;
-// devs = 'true';
+// let devs: string | boolean;
+// devs = 'work';
 // console.log(devs);
 /**
- * @description TypeScript function learn
- * to use optional parameter, give it a default value
- * use void type when a function returns nothing like console.log
- * use never type when a function never completes like throwing error and infinite loop
+ * @void_and_never_Function_learn in TypeScript
+ * - to use optional parameter, give it a default value
+ * - use void when nothing returns ex: console.log
+ * - use never when never completes ex: throwing error and infinite loop
  */
 // const voidLearn = (name: string, age: number): void => {
 //   console.log(`My name is ${name}, I'm ${age} years old.`);
 // };
-// myAge('work', 29);
+// voidLearn('toquir ahmed topu', 30);
 // const neverLearn = (name: string, age: number): never => {
+//   throw new Error('Nothing implemented yet');
 //   while (true) {
 //     console.log(`My name is ${name}, I'm ${age} years old.`);
 //   }
 // };
 /**
- * @desc TypeScript array and tuple array learn
- * can't push different type of data type in array
+ * @array_and_tuple learn in TypeScript
+ * - can't push other than declared type in array
+ * - array destructure: let [val1, val2] = arrayName
+ * - to swap variables type has to be same: [val1, val2] = [val2, val1]
+ * - can spread too: let [val1, ...rest] = arrayName
  */
-// const car: string[] = ['Audi', 'Mercedes', 'Toyota', 'Honda'];
-// car.push(30);
+// const car: (string | number)[] = ['Audi', 'Mercedes', 'Toyota', 'Honda'];
+// car.push('bmw');
 // console.log(car);
-// tuple
-// const devs: [string, number, boolean] = ['John Doe', 20, true];
-// console.log(devs);
+// const devs: [string, number, boolean] = ['toquir ahmed', 84, true];
+// let [first, ...rest] = devs;
+// first = 'toquir ahmed topu';
+// rest[0] = 42;
+// console.log(rest);
 /**
- * @desc TypeScript object learn
- * @optional use ? to make a property optional
- * @readonly to make a property not assignable
- * @separateType use when there is lots of properties in the object
+ * @object learn in TypeScript
+ * - use ? when unsure about that value in type
+ * - readonly can't write data
+ * - `type alias` use when there is lots of properties in the object
  */
+// const object: { readonly name: string; age: number; isLogin?: boolean } = {
+//   name: 'toquir ahmed',
+//   age: 42,
+//   isLogin: true,
+// };
+// object.isLogin = false;
+// console.log(object);
 // type Car = {
-//   brand: string;
+//   readonly brand: string;
 //   model: string;
 //   year?: number;
 //   isUsed?: boolean;
+//   car: {};
 // };
 // interface CarInterface {
 //   brand: string;
 //   model: string;
 //   year?: number;
 //   isUsed?: boolean;
+//   car: {
+//     name: string;
+//     age: number;
+//   };
 // }
 // const myCar: CarInterface = {
 //   brand: 'Audi',
 //   model: 'A4',
 //   year: 2020,
+//   car: {
+//     name: 'bmw',
+//     age: 28,
+//   },
 // };
 // myCar.year = 2022;
-// console.log(myCar.year);
+// console.log(myCar);
 /**
- * @desc TypeScript enum learn
+ * @enum_and_type_narrowing learn in TypeScript
  */
-// enum skills {
-//   MERN = 'MERN stack',
-//   GO = 'GO stack',
-//   PYTHON = 'PYTHON stack',
-//   RUBY = 'RUBY stack',
-// }
-// const skill = skills.RUBY;
-// console.log(skill);
-// // type narrowing
+var skills;
+(function (skills) {
+    skills["MERN"] = "MERN stack";
+    skills["GO"] = "GO stack";
+    skills["PYTHON"] = "PYTHON stack";
+    skills["RUBY"] = "RUBY stack";
+})(skills || (skills = {}));
+const skill = skills.MERN || skills.GO;
+console.log(skill);
+// type narrowing
 // const info = (val: string | number) => {
 //   if (typeof val === 'string') {
 //     return val.toUpperCase();
